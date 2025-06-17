5468532bcf1befb1862befcddc9c2b083563b0f0
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 19:07:02 +0530

    Update dowhile.java

--- dowhile.java
+++ dowhile.java
@@ -2,7 +2,7 @@ public class Dowhile {
     public static void main(String[] args) {
         int i = 1;
         do {
-            System.out.println("Saaili");
+            System.out.println("Anay");
             i++;
         } while (i <= 5);
     }
