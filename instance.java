753a047fe1d62cb410f40835c9b4ebc53547e12f
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 19:08:28 +0530

    Update instance.java

--- instance.java
+++ instance.java
@@ -1,6 +1,6 @@
 public class Instance {
     public static void main(String[] args) {
-        String name = "Saaili";
+        String name = "Anay";
         System.out.println("name instanceof String: " + (name instanceof String));
         System.out.println("name instanceof Object: " + (name instanceof Object));
     }
