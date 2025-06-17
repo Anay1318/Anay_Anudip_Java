d431f9afc4d465d0aadc2855f54b64e5296224c8
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 19:10:41 +0530

    Update whileloop.java

--- whileloop.java
+++ whileloop.java
@@ -2,7 +2,7 @@ public class WhileLoopSaaili {
     public static void main(String[] args) {
         int i = 1;
         while (i <= 5) {
-            System.out.println("Saaili");
+            System.out.println("Anay");
             i++;
         }
     }
