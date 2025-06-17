772bb765259ada6f01d0727706e3a840357384d4
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 19:12:27 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,6 +1,6 @@
 public class arrayfun {
     public static void main(String[] args) {
-        String[] names = {"Anay", "Focalors", "Surtalogi"};
+        String[] names = {"Anay", "Aquilla", "Cygnus"};
 
         System.out.println(names[0]);
         System.out.println(names[1]);
