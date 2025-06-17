0603067b2e879ed9cb6a9c1d973ca55f3844699e
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 17:31:19 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,12 @@
+public class Function {
+    public static int Addition(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        int result = Addition(a, b);
+        System.out.println("Result: " + result);
+    }
+}
