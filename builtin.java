31f352dcc6197b28d533236e11a978359a0ae1d3
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 17:33:17 +0530

    Create builtin.java

---
+++ builtin.java
@@ -0,0 +1,10 @@
+public class Builtinfun {
+    public static void main(String[] args) {
+        String name = "Anay";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Max of 5 and 10: " + Math.max(5, 10));
+    }
+}
