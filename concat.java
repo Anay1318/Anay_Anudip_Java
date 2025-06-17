36ece23046109e06b3535029b714488672353cf5
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 19:06:40 +0530

    Update concat.java

--- concat.java
+++ concat.java
@@ -1,7 +1,7 @@
 public class Concatenation {
     public static void main(String[] args) {
-        String a = "Saaili";
-        String b = "Amborkar";
+        String a = "Anay";
+        String b = "Deshmukh";
         String c;
         c = a + b;
         System.out.println("The value of c is: " + c);
