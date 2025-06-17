8f41d2f296533baa8a0f4292707ca484a2fbef56
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 17:31:48 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,10 @@
+public class Function {
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(4, 5);
+        System.out.println("Multiplication is: " + result);
+    }
+}
