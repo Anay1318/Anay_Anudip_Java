b9a8907ee596994cb8b2fbc76c79cda4d403d5ea
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 17:29:16 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,9 @@
+public class WhileLoopSaaili {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Saaili");
+            i++;
+        }
+    }
+}
