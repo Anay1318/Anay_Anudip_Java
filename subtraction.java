5b7698cee7cbb4ce28c3359309a93051af7f0990
Anay1318 <anaydeshmukh1318@gmail.com>
2025-06-17 17:30:45 +0530

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,12 @@
+public class Function {
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        int result = subtract(a, b);
+        System.out.println("Result: " + result);
+    }
+}
